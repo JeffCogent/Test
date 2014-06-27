b166aad44c51de7db0ec1cd635ae48f1212e553b
Jeff Lawson <jdl@cogentlogic.com>
2014-06-27 13:00:28 +0100

    Initial commit from local repo

---
+++ GitHub/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GitHub
+//
+//  Created by Jeffrey Lawson on 27/06/2014.
+//  Copyright (c) 2014 Cogent Logic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
